7c15809bf2de5ab3b9d9b4491f43c0edae1b5239
luffy977338 <timur.djeletyan.2009@gmail.com>
2025-09-30 21:42:49 +0300

    fix: ws\

--- client/src/hooks/useChartSocket.js
+++ client/src/hooks/useChartSocket.js
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 
-const SOCKET_URL = "http://localhost:8000";
+const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const INTERVAL_MAP = {
   "1m": "1",
