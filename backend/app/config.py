9926639461f559c2d029f3260b917f7299270288
timakaa <timur.djeletyan.2009@gmail.com>
2025-09-30 17:09:31 +0300

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -21,9 +21,8 @@ class Settings:
     # CORS Settings
     BACKEND_CORS_ORIGINS: list = [
         "http://localhost:3000",
-        "http://localhost:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
+        "http://192.168.1.65:3000", 
     ]
 
     # Security Settings
