3531ab043ed088fb84e12df85bda66095f28d400
luffy977338 <timur.djeletyan.2009@gmail.com>
2026-03-04 03:19:52 +0300

    fix: dont save drawing if its drawn in backtest

--- client/src/drawing-tools/long-position/index.js
+++ client/src/drawing-tools/long-position/index.js
@@ -52,6 +52,9 @@ export class LongPositionDrawingTool extends BasePositionDrawingTool {
 
   // Save long position data to persistent store
   _saveLongPositionToStore(position) {
+    // Don't save drawings when in backtest mode
+    if (window.location.pathname.startsWith("/backtest/")) return;
+
     const { addDrawing } = useDrawingsStore.getState();
     const { ticker } = useChartStore.getState();
 
