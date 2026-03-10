7a16b23d786c68906a752722d427087972528f0a
luffy977338 <timur.djeletyan.2009@gmail.com>
2026-03-10 08:41:39 +0300

    feat: basic typescript setup + socket logic refactored

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -24,10 +24,7 @@ export default [
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "no-unused-vars": ["warn", { varsIgnorePattern: "^[A-Z_]" }],
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": "off",
       "react-hooks/exhaustive-deps": "off",
     },
   },
---
+++ client/src/constants/chart.js
@@ -0,0 +1,24 @@
+export const INTERVAL_MAP = {
+  "1m": "1",
+  "3m": "3",
+  "5m": "5",
+  "15m": "15",
+  "30m": "30",
+  "1h": "60",
+  "2h": "120",
+  "4h": "240",
+  "6h": "360",
+  "12h": "720",
+  "1d": "D",
+  "1w": "W",
+  "1M": "M",
+};
+
+export const SOCKET_EVENTS = {
+  CHART_DATA_UPDATED: "chart_data_updated",
+  CHART_DRAWING_RECEIVED: "chart_drawing_received",
+  CHART_DRAWING_UPDATED: "chart_drawing_updated",
+  CHART_DRAWING_DELETED: "chart_drawing_deleted",
+  JOIN_ROOM: "join_room",
+  LEAVE_ROOM: "leave_room",
+};
